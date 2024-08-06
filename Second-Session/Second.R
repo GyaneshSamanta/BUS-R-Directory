84811277bb5ac335efb14a8fee52753fbf952ac3
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2024-08-06 16:17:30 +0530

    add: Plots

--- Second-Session/Second.R
+++ Second-Session/Second.R
@@ -10,6 +10,24 @@ plot(x,y,type="l", col="blue")
 plot(x, y, type = "l", col = "red", main = "X ~ N(0, sd=1)", lwd = 2)
 
 
-# to learn normal curve and probabilties 
 
-#create 
+
+
+# Create a sequence of x values from -3 to 3 with an increment of 0.01
+x <- seq(from = -3, to = 3, by = 0.01)
+
+# Calculate the y values for the standard normal distribution
+y <- dnorm(x, mean = 0, sd = 1)
+
+# Plot the standard normal curve
+plot(x, y, type = "l", col = "red", main = "X ~ N(0, sd=1)", lwd = 2)
+
+# Shade the area under the curve
+
+polygon(c(-3,x,0.24) , c(0,y,0),col="red")
+
+ 
+x <- seq(from=-3 , to=0.24, by=0.1)
+y <- dnorm(x,mean=0,sd=1)
+plot(x,y,type= "l" , col = "red")
+polygon(c(-3, x,0.24), c(0,y,0) , col="red")
