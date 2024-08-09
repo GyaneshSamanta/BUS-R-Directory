01d72ca063c57d83db4c4c03afc8b02e67f6abff
Gyanesh Samanta <mail.gyaneshsamanta@gmail.com>
2024-08-09 12:12:02 +0530

    feat:third session

--- Third-Session/third.R
+++ Third-Session/third.R
@@ -12,6 +12,16 @@ x_subset <- x[x >= -1.52 & x <= 1.96]
 y_subset <- y[x >= -1.52 & x <= 1.96]
 
 # Shade the area under the curve from -1.52 to 1.96 using the subset of y
-polygon(c(-1.52, x_subset, 1.96),
+#polygon(c(-1.52, x_subset, 1.96),
+#        c(0, y_subset, 0),
+#        col = "red")
+
+
+
+x_subset <- x[x >= 1.52 & x <= 2.5]
+y_subset <- y[x >= 1.52 & x <= 2.5]
+
+# Shade the area under the curve from -1.52 to 1.96 using the subset of y
+polygon(c(1.52, x_subset, 2.5),
         c(0, y_subset, 0),
         col = "red")
